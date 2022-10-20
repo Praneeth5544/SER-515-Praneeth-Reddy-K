ea6f4cafbc8e819fdf1e96852e85a55bc904f750
Praneeth5544 <pkancha2@asu.edu>
2022-10-20 15:44:35 -0700

    Main method calling Seller/Login functions of facade class implemented successfully

---
+++ App.java
@@ -0,0 +1,12 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Facade fp= new Facade();
+        fp.login();
+        
+
+    }
+}
