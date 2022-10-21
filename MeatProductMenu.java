ae6b5056f77993323167148dd025dec893634a34
Praneeth5544 <pkancha2@asu.edu>
2022-10-20 17:33:42 -0700

    This class implements show menu items of only Meat Items

---
+++ MeatProductMenu.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class MeatProductMenu implements ProductMenu {
+
+	public void showMenu() {
+		try{
+			//File reader to read the content of file
+		FileReader fr = new FileReader("C:\\Users\\hp\\Desktop\\SER 515\\Assignment-2-Ind\\SER-515-Praneeth-Reddy-K\\TextFiles\\ProductInfo.txt");
+		BufferedReader br=new BufferedReader(fr);
+		String st;
+		ArrayList<String> MeatItemList= new ArrayList<String>();
+        while ((st = br.readLine()) != null)
+		{
+			// Tokenizer to separate the words as tokens with a delimitor :
+			// Adding all Produce Items into ProduceItemList
+		StringTokenizer s =new StringTokenizer(st,":");
+		   while(s.hasMoreTokens())
+		   {
+			String Item=s.nextToken();
+			String Value=s.nextToken();
+			if(Item.equals("Meat"))
+			MeatItemList.add(Value);
+		   }      
+		}
+		//Displaying the Produce Items List
+		System.out.println("Meat Items List");
+		for(int i=0;i<MeatItemList.size();i++)
+		{
+			System.out.println("Meat :"+MeatItemList.get(i));
+		}
+
+		}
+		catch (FileNotFoundException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void showAddButton() {
+
+	}
+
+	public void showViewButton() {
+
+	}
+
+	public void showRadioButton() {
+
+	}
+
+	public void showLabels() {
+
+	}
+
+	public void showComboxes() {
+
+	}
+
+}
