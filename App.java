e01ef7a037c2c828a8370d2c63c7430336e451a9
Praneeth5544 <pkancha2@asu.edu>
2022-10-20 17:31:36 -0700

    Main Method calling showItems function of MeatProductMenu Class upon successfull login

--- App.java
+++ App.java
@@ -3,11 +3,14 @@ import java.io.IOException;
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        Facade fp= new Facade();
         ProduceProductMenu ppm =new ProduceProductMenu();
+        MeatProductMenu mpm = new MeatProductMenu();
         if(fp.login())
+        {
         ppm.showMenu();
+        mpm.showMenu();
+        }
+
 
     }
 }
