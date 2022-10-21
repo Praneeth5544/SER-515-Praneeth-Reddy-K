1b68add592dfc879c941ff5860a0a0484f59ee1e
Praneeth5544 <pkancha2@asu.edu>
2022-10-20 21:56:34 -0700

    This class implements show menu items of only Buyer Items

--- App.java
+++ App.java
@@ -2,18 +2,51 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 public class App {
+
+    public void MoreItems()
+    {
+            ProduceProductMenu ppm =new ProduceProductMenu();
+            MeatProductMenu mpm = new MeatProductMenu();
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Bridge Pattern Implemented");
+            System.out.println("--------------------------");
+            System.out.println("Enter: 1. MeatProductMenu 2. ProduceProductMenu");
+            int opt=sc.nextInt();
+            if(opt==1)
+            {
+                mpm.showMenu(); 
+            }
+            else
+            {
+                ppm.showMenu();
+            }
+    }
     public static void main(String[] args) throws Exception {
-        ProduceProductMenu ppm =new ProduceProductMenu();
-        MeatProductMenu mpm = new MeatProductMenu();
+        Scanner sc = new Scanner(System.in);
+        // ProduceProductMenu ppm =new ProduceProductMenu();
+        // MeatProductMenu mpm = new MeatProductMenu();
         Facade fp= new Facade();
-        Person p =new Person();
-        Seller s= new Seller();
+        // Person p =new Person();
+        // Seller s= new Seller();
         if(fp.login())
         {
-        ppm.showMenu();
-        mpm.showMenu();
-        p.showMenu();
-        s.showMenu();
+            System.out.println("Bridge Pattern Implemented");
+            System.out.println("--------------------------");
+            App p = new App();
+            p.MoreItems();
+            // System.out.println("Enter: 1. MeatProductMenu 2. ProduceProductMenu");
+            // int opt=sc.nextInt();
+            // if(opt==1)
+            // {
+            //     mpm.showMenu(); 
+            // }
+            // else
+            // {
+            //     ppm.showMenu();
+            // }
+
+       // p.showMenu();
+        //s.showMenu();
         }
 
 
