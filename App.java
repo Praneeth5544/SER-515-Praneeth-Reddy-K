821e50c6781a6a589537e73f981f2f95c1af110e
Praneeth5544 <pkancha2@asu.edu>
2022-10-20 22:01:23 -0700

    Main Method calling showItems function of Buyer Class upon successfull login

--- App.java
+++ App.java
@@ -10,7 +10,7 @@ public class App {
             Scanner sc = new Scanner(System.in);
             System.out.println("Bridge Pattern Implemented");
             System.out.println("--------------------------");
-            System.out.println("Enter: 1. MeatProductMenu 2. ProduceProductMenu");
+            System.out.println("Enter: 1. MeatProductMenu 2. ProduceProductMenu ");
             int opt=sc.nextInt();
             if(opt==1)
             {
@@ -23,30 +23,13 @@ public class App {
     }
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        // ProduceProductMenu ppm =new ProduceProductMenu();
-        // MeatProductMenu mpm = new MeatProductMenu();
         Facade fp= new Facade();
-        // Person p =new Person();
-        // Seller s= new Seller();
         if(fp.login())
         {
             System.out.println("Bridge Pattern Implemented");
             System.out.println("--------------------------");
             App p = new App();
             p.MoreItems();
-            // System.out.println("Enter: 1. MeatProductMenu 2. ProduceProductMenu");
-            // int opt=sc.nextInt();
-            // if(opt==1)
-            // {
-            //     mpm.showMenu(); 
-            // }
-            // else
-            // {
-            //     ppm.showMenu();
-            // }
-
-       // p.showMenu();
-        //s.showMenu();
         }
 
 
