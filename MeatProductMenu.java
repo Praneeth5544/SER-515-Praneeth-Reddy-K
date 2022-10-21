86e38a7f2c7b8301adb7045b5b74661ad3c5d4ee
Praneeth5544 <pkancha2@asu.edu>
2022-10-20 22:19:15 -0700

    This class implements show menu items of MeatProductMenu class

--- MeatProductMenu.java
+++ MeatProductMenu.java
@@ -3,12 +3,15 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class MeatProductMenu implements ProductMenu {
 
 	public void showMenu() {
 		try{
+			Scanner sc= new Scanner(System.in);
+			Person p= new Person();
 			//File reader to read the content of file
 		FileReader fr = new FileReader("C:\\Users\\hp\\Desktop\\SER 515\\Assignment-2-Ind\\SER-515-Praneeth-Reddy-K\\TextFiles\\ProductInfo.txt");
 		BufferedReader br=new BufferedReader(fr);
@@ -28,11 +31,18 @@ public class MeatProductMenu implements ProductMenu {
 		   }      
 		}
 		//Displaying the Produce Items List
+		System.out.println("Factory Pattern Implemented");
+		System.out.println("------------------------");
+		System.out.println("Select Items From List");
+		System.out.println("-----------------------");
 		System.out.println("Meat Items List");
 		for(int i=0;i<MeatItemList.size();i++)
 		{
-			System.out.println("Meat :"+MeatItemList.get(i));
+			System.out.println((i+1)+" Meat :"+MeatItemList.get(i));
+			
 		}
+		int item=sc.nextInt();
+			p.showMenu();
 
 		}
 		catch (FileNotFoundException e) 
